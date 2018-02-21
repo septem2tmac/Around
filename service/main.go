b196fe4b5d4ed4c3af1d53043ba2031b0d8780c1
septem2tmac <septem2tmac@gmail.com>
2018-02-20 22:38:49 -0800

    change URL

--- service/main.go
+++ service/main.go
@@ -37,7 +37,7 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.227.20.124:9200"
+	ES_URL = "http://35.196.144.185:9200"
 	// Needs to update this bucket based on your gcs bucket name.
 	BUCKET_NAME = "post-images-090909"
 
